03f9d51bb8f736b02fb1be9fa7b6433fd7a2849e
E518553 <lukas.pestalozzi@sbb.ch>
2019-07-30 10:52:06 +0200

    added connection to passwordsfile and some improvements

--- call_jmx_method.py
+++ call_jmx_method.py
@@ -7,17 +7,15 @@ import logging
 from file_utils import *
 
 logging.getLogger().setLevel(logging.DEBUG)
+NEWLINE = os.linesep
 
-XAVER_JMX_CONNECTION_URI = "service:jmx:rmi:///jndi/rmi://rcsxaver1:2099/rcs"
-
-JMX_USER = file_to_lines("./passwords")[0].strip()
-JMX_PASSWORD = file_to_lines("./passwords")[1].strip()
-
-print(JMX_USER, JMX_PASSWORD)
-
-NEWLINE = "\n"
-
-CON_JMX_XAVER = JMXConnection(connection_uri=XAVER_JMX_CONNECTION_URI, jmx_username=JMX_USER, jmx_password=JMX_PASSWORD)
+# Read the passwords file
+passwordlines = file_to_lines("./passwords", strip_=True)
+JMX_CONNECTION_URI = passwordlines[0]
+JMX_USER = passwordlines[1]
+JMX_PASSWORD = passwordlines[2]
+#print(XAVER_JMX_CONNECTION_URI, JMX_USER, JMX_PASSWORD)
+JMX_CONNECTION = JMXConnection(connection_uri=JMX_CONNECTION_URI, jmx_username=JMX_USER, jmx_password=JMX_PASSWORD)
 
 
 def query_all_fwr_with_nodeId(nodeId: str):
@@ -30,20 +28,23 @@ def query_all_fwr_with_nodeId(nodeId: str):
 
 def method_fwr_on_nodeId(nodeId: str, methodName: str, params: List[JMXParam] = None):
     """
-
     :param nodeId: example: K57077.2028
     :param methodName: example: getStoredPotenziellRelevanteZugfahrtenIds
     :param params:
     :return:
     """
-    return JMXMethod(
-        f"Processes:name=FahrwegRechner,type=ServerProcess,process=FahrwegRechner,nodeId={nodeId}",
-        methodName, params=params if params else list())
+    try:
+        return JMXMethod(
+            f"Processes:name=FahrwegRechner,type=ServerProcess,process=FahrwegRechner,nodeId={nodeId}",
+            methodName, params=params if params else list())
+    except Exception as e:
+        print(f"exception wile calling JMXmethod. Does the nodeId {nodeId} exist?")
+        raise e
 
 
 def call_single_query(query: JMXQuery, print_result=False):
     jmx_query = [query]
-    metrics = CON_JMX_XAVER.query(jmx_query)
+    metrics = JMX_CONNECTION.query(jmx_query)
     result_string = f"received {len(metrics)} metrics:"
     for metric in metrics:
         result_string += f"\n{metric.to_query_string()} ({metric.value_type}) = {metric.value}"
@@ -53,17 +54,20 @@ def call_single_query(query: JMXQuery, print_result=False):
 
 
 def call_single_method(method: JMXMethod, print_result=False):
-    result = CON_JMX_XAVER.call(method)
+    result = JMX_CONNECTION.call(method)
     if print_result:
         print(f"received result: {result}")
     return result
 
 
 def test_connection():
+    print("testing connection to ", JMX_CONNECTION.connection_uri, "...")
     jmx_query = [JMXQuery("*:*/HeapMemoryUsage")]
-    metrics = CON_JMX_XAVER.query(jmx_query)
+    metrics = JMX_CONNECTION.query(jmx_query)
     if len(metrics):
         print(f"received {len(metrics)} metrics. Connection OK :)")
+    else:
+        print("got response but no metrics:", metrics, "Probably NOT ok")
 
 
 def call_methods_and_save(methods: Union[JMXMethod, List[JMXMethod]], filepath: str,
@@ -82,7 +86,7 @@ def call_methods_and_save(methods: Union[JMXMethod, List[JMXMethod]], filepath: 
 
 
 if __name__ == '__main__':
-
+    test_connection()
     """
     # call_single_query(query_all_fwr_with_nodeId(XAVER_NODE_ID))
     print(f"====================== {MY_NODE_ID} ===========================")
